8f33b9494020ac8e7d91d4508c9085dcde6fdd69
zhen-0712 <zhangyongzhen261@gmail.com>
2023-11-23 20:04:40 +0800

    Site updated: 2023-11-23 20:04:37

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+    autoplay: false,
+    theme: '#b7daff',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: '200px',
+    lrcType: 3,
+    audio: [
+      {
+        name: "麵包",
+        artist: 'peaceful',
+        url: 'images/Music.mp3',
+        cover: 'images/test.png'
+      }
+    ]
+});
